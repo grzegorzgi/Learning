30d99a215416d61cd924ee9e3018da8caf26bef8
GrzegorzGiniewicz <200949@student.pwr.edu.pl>
2019-03-02 23:08:01 +0100

    pierwsze proby

---
+++ Cmdlets adv.ps1
@@ -0,0 +1,22 @@
+﻿<#get-unique
+
+
+#$list = "one","two","two","three","four","five"
+#$list
+$list | sort | Get-Unique
+
+
+#>
+
+
+#Measure-Object - liczy pliki, linie, słowa, znaki etc.
+#get-content 'C:\Users\Grzegorz\Desktop\PW\2.txt' | measure-object -character -line -word
+#Get-ChildItem C:\Users\Grzegorz\Desktop\PW | Measure-Object ---> liczy pliki
+
+
+#Compare-Object porownywanie obiektów
+Compare-Object -ReferenceObject $(Get-Content C:\Users\Grzegorz\Desktop\PW\1.txt) -DifferenceObject $(Get-Content C:\Users\Grzegorz\Desktop\PW\2.txt)
+
+#Format-List - formatuje dane do listy i wyświetla
+Get-Service | Format-List
+
---
+++ Loops.ps1
@@ -0,0 +1,37 @@
+﻿#FOR
+
+For ($i=0; $i -le 5; $i++) {
+    Write-Host "Iteracja $i"
+}
+
+
+#ForEach
+$array ="a", "b", "c", "d", "e"
+Foreach ($i in $array) {
+    Write-Host "Literka: $i"
+}
+
+#ForEach-Ooject
+Get-ChildItem C:\Users\Grzegorz\Desktop\PW | ForEach-Object {write-host ("Nazwa: " + $_.FullName)}
+
+# While loop
+$x = 0
+while ($x -lt 10) {
+    Write-Host $x
+    $x++
+}
+
+# Do while
+$y = 0
+Do {
+    $y++
+    Write-Host $y
+}while($y -lt 5)
+
+
+# Do until
+$y = 10
+Do {
+    $y++
+    Write-Host $y
+}Until($y -gt 5)
