2ae4382965e70e7c227ff827b94fe74ba3aef583
GrzegorzGiniewicz <200949@student.pwr.edu.pl>
2019-03-03 10:27:08 +0100

    adding array sum script

---
+++ Training/array+foreach_sum.ps1
@@ -0,0 +1,9 @@
+﻿$myArray = 1,2,3,4,5,6,7,8,9,10,11,20,30,29;
+[int] $sum =0;
+
+foreach ($val in $myArray)
+{
+    write-host ("Index with Value: $val")
+    $sum = $sum + $val
+}
+write-host ("the sum is: $sum")
