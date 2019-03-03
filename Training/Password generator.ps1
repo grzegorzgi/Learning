1f5a563177571caaf04123fb9230887eb8d2befd
GrzegorzGiniewicz <200949@student.pwr.edu.pl>
2019-03-03 14:28:53 +0100

    pass generator and guess the letter

---
+++ Training/Password generator.ps1
@@ -0,0 +1,30 @@
+﻿$Z1 = (65..100) + (65..110) | Get-Random -Count 1 | ForEach-Object {[CHAR]$_}
+$Z2 = (65..100) + (65..110) | Get-Random -Count 1 | ForEach-Object {[CHAR]$_}
+$Z3 = (65..100) + (65..110) | Get-Random -Count 1 | ForEach-Object {[CHAR]$_} 
+$Z4 = (65..100) + (65..110) | Get-Random -Count 1 | ForEach-Object {[CHAR]$_} 
+$Z5 = (65..100) + (65..110) | Get-Random -Count 1 | ForEach-Object {[CHAR]$_} 
+$Z6 = (65..100) + (65..110) | Get-Random -Count 1 | ForEach-Object {[CHAR]$_} 
+Write-Host $Z1$Z2$Z3$4$Z5$Z6
+
+
+$z = read-host "Write length of password"
+$pw = [String]::Format(-join ((65..90) + (65..90) | Get-Random -Count $z | ForEach-Object {[CHAR]$_}))
+Write-Host $pw
+
+
+
+
+$z = read-host "Write length of password"
+
+For ($i=1; $i -le $z; -$i++) {
+ (65..90) + (65..90) | Get-Random -Count 1 | ForEach-Object {[CHAR]$_}
+  
+}
+
+
+$pw = "M"
+while($pw -ne $guess)
+{
+  $guess = (-join ((65..90) + (65..90) | Get-Random -Count 1 | ForEach-Object {[CHAR]$_}))
+  Write-Host $guess
+}
---
+++ Training/Randorm try.ps1
@@ -0,0 +1,14 @@
+﻿$a = 1,2,34,5,6,7,8,9,10;
+[int] $sum = 0;
+
+foreach($element in $a)
+{
+    $sum = $sum+$element
+}
+Write-host $sum
+
+
+for($z =0; $z -le 20; $z++){
+
+    write-host "LOOP $z"
+}
