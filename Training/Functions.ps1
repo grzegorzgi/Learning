aaa0b4cbd7cbafbc229b22f5da2bdbcc058b9274
GrzegorzGiniewicz <200949@student.pwr.edu.pl>
2019-03-03 18:46:39 +0100

    functions

---
+++ Training/Functions.ps1
@@ -0,0 +1,16 @@
+﻿Function ReturnString ($p)
+{
+    $p = $p + 1
+    return $p
+}
+Write-Host "Result :"
+ReturnString -p 10
+
+
+Function ReturnString ($z1, $z2)
+{
+    $x = $z1 + " " + $z2
+    return $x
+}
+Write-Host "Result :"
+ReturnString -z1 "Hello" -z2 "World!" 
