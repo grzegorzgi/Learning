660740fada15c87eaaae7e0d21fe031f84566287
GrzegorzGiniewicz <200949@student.pwr.edu.pl>
2019-03-03 09:56:34 +0100

    adding guess game and folder existance chceck

---
+++ Guess the number game.ps1
@@ -0,0 +1,13 @@
+﻿$RandomNumber = Get-Random -Minimum 1 -Maximum 10
+$TryAgain  = "Y"
+while($TryAgain.ToUpper() -eq "Y") {
+    $Guess = Read-Host "Enter your guess"
+    if ($Guess -eq $RandomNumber) {
+    Write-Host "You Win!"
+    }
+    else
+    {
+    Write-host "Wrong!"
+    $TryAgain = Read-Host "Again? Press y"
+    }
+}
---
+++ Training/Check if file exist.ps1
@@ -0,0 +1,9 @@
+﻿$Path = Test-Path 'C:\Users\Grzegorz\Desktop\PW'
+if ($Path -eq $True)
+{
+Write-host "Folder Exist!"
+}
+else
+{
+Write-Host "There is no such file!"
+}
