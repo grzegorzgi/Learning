70994251c6d370e5508ef4d28269e73d1a93b883
GrzegorzGiniewicz <200949@student.pwr.edu.pl>
2019-03-03 12:05:02 +0100

    arrays

---
+++ Training/Array.ps1
@@ -0,0 +1,52 @@
+﻿#Array
+$myArray = 5.6, 4, 17, 1, 13,7, 3, 100
+$myList = @(0..4)
+Write-host $myList
+
+write-host("Print list elements")
+$myArray
+
+write-host("2 element in array")
+$myArray[3]
+
+write-host("0-3 elements in array")
+$myArray[0..3]
+
+write-host("0-3 elements in array")
+$subArray = $myArray[0..3]
+
+write-host("using for loop")
+for ($i = 0; $i -le ($myArray.length); $i += 1) {
+  $myArray[$i]
+}
+
+write-host("foreach loop array")
+foreach ($element in $myArray){
+$element
+}
+
+write-host "while loop array"
+$w=0
+while($w -lt 8){
+$myArray[$w]
+$w++
+}
+
+write-host "while loop array back"
+$w=8
+while($w -ge 0){
+$myArray[$w]
+$w--
+}
+
+$myArray = 5.6, 4, 17, 1, 13,7, 3, 100
+$myArray.GetType()
+write-host ($myArray).Length
+write-host $myArray
+$myArray[7] = 1000
+$myArray += 106
+[System.Collections.ArrayList]$ArrayList = $myArray
+$ArrayList.GetType()
+
+Write-host $myArray
+
---
+++ Training/array writing loop.ps1
@@ -0,0 +1,21 @@
+﻿$stop = "Y"
+$AddArray = New-Object System.Collections.ArrayList
+#write-host $AddArray.Length
+[System.Collections.ArrayList]$ArrayList = $AddArray
+while($stop.ToUpper() -eq "Y")
+{
+    if($add -eq "s"){
+     Write-host "Stopped!"
+     break
+
+    }
+
+    else{
+    Write-Host "To stop press 's'"
+    $Add = Read-Host "Write number that will be added to array"
+    $AddArray += $Add
+    write-host $AddArray
+    }
+}
+
+
